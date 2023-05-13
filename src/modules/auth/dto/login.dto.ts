0c1ec3303d2e46b190de6cff17053adfa30e9694
Juan Pablo Victoria <lider.backend@talentum.edu.co>
2023-05-13 12:09:53 -0500

    feat: Ajusted LoginDto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,11 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty()
   @IsNotEmpty()
-  @IsString()
-  @Length(2, 20)
+  @IsEmail()
   email: string;
 
   @ApiProperty()
