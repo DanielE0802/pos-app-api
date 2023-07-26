9b5132457427d14359e4e620a3419751e6bf762f
Juan Pablo VC <44125388+juanp288@users.noreply.github.com>
2023-07-26 20:37:16 +0000

    hotfix: Validate company in Decorator

--- src/modules/auth/decorators/get-user.decarator.ts
+++ src/modules/auth/decorators/get-user.decarator.ts
@@ -1,15 +1,32 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { IRelationType } from 'src/common/decorators/relation.decorator';
 
 export const GetUser = createParamDecorator((_data, ctx: ExecutionContext) => {
   const { user } = ctx.switchToHttp().getRequest();
+
+  if (!user)
+    throw new UnprocessableEntityException('Token does not contain a user');
+
   delete user.password;
+
+  console.log(user)
+
   return user;
 });
 
 export const GetUserCompany = createParamDecorator(
   (_data, ctx: ExecutionContext) => {
     const { user } = ctx.switchToHttp().getRequest();
+
+    if (!user.profile.company)
+      throw new UnprocessableEntityException('User has no companies assigned');
+
+    console.log(user.profile.company)
+
     return user.profile.company as IRelationType;
   },
 );
