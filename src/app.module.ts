d1c7072e8cb03ff3e58e836a5471072574e50f56
Juan Pablo Victoria <jpv288@gmail.com>
2023-07-25 19:13:41 -0500

    feat: Remove PassportModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,12 @@ import { dbConfig } from './common/config/db.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductModule } from './modules/product/product.module';
 import { LocationModule } from './modules/location/location.module';
-import { PdvModule } from './modules/pdv/pdv.module';
 import { CompanyModule } from './modules/company/company.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtConfig } from './common/config/jwt.config';
-import { JwtStrategy } from './modules/auth/jwt/jwt.strategy';
-import { UserProviders } from './modules/user/providers/user.providers';
-import { UsersService } from './modules/user/services/user.service';
-import { User } from './modules/user/entities/user.entity';
 
 @Module({
   imports: [
-    PassportModule.register({ defaultStrategy: JwtConfig.strategy }),
     TypeOrmModule.forRoot({
       type: dbConfig.dbClient,
       host: dbConfig.dbHost,
