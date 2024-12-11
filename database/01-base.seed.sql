19ecbb91147b5a084b95ecba681fa1921da8066c
Juan Pablo <juan.pablo.victoria.caro@theflock.dev>
2024-12-11 13:32:38 -0500

    chore: Update data seed

--- database/01-base.seed.sql
+++ database/01-base.seed.sql
@@ -3,7 +3,10 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- INSERT DATA SEED
+INSERT INTO `departments` (`id`, `name`) VALUES
+(1, 'Departamento A'),
+(2, 'Departamento B'),
+(3, 'Departamento C');
 
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
